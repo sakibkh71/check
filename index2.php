882a7c208d1cedb3f9e4902a19641625f62e936f
sakibkh <sakibkh71@gmail.com>
2017-02-03 23:30:09 +0600

    another chk conflict

--- index2.php
+++ index2.php
@@ -1,8 +1,6 @@
 <?php 
 
 
-<<<<<<< HEAD
 echo "index2 check annnother change2 from check sakib branch";
-=======
+echo "one part time try to making conflict";
 
->>>>>>> b3a79ff21aa137697087981ec27ff9cd98c26e84
